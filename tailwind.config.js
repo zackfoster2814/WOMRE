acebd2770f27ef8018b5545ff937b2b25e8467f1
zackfoster2814 <tuankhoi240202@gmail.com>
2025-08-28 18:22:55 +0700

    feature: Create new UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,46 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  content: [
+    "./index.html",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        handwriting: ["'Caveat', cursive"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addUtilities }) {
+      addUtilities({
+        ".chart-grid": {
+          display: "grid",
+          "grid-template-columns": "repeat(1, minmax(0, 1fr))",
+          gap: "1rem",
+          "@media (min-width: 768px)": {
+            "grid-template-columns": "repeat(2, minmax(0, 1fr))",
+            "& > *:last-child:nth-child(odd)": {
+              "grid-column": "1 / -1",
+            },
+          },
+        },
+        ".scrollbar-hide": {
+          /* IE and Edge */
+          "-ms-overflow-style": "none",
+          /* Firefox */
+          "scrollbar-width": "none",
+          /* Safari and Chrome */
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        },
+      });
+    },
+  ],
 };
