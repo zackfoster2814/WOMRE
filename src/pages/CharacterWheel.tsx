76bc735f7fd428adae1118427ca4ea35f0065fd5
zackfoster2814 <tuankhoi240202@gmail.com>
2025-09-07 23:36:39 +0700

    Feature: Update UI

--- src/pages/CharacterWheel.tsx
+++ src/pages/CharacterWheel.tsx
@@ -7,6 +7,8 @@ import { CenterWheel } from "@/components/CenterWheel.tsx";
 import { RightPanel } from "@/components/RightPanel.tsx";
 import { CharacterDataDialog } from "@/components/CharacterDataDialog.tsx";
 
+import wheelBg from "../assets/Backgrounds/wheel-bg.png";
+
 export default function CharacterWheel() {
   // Custom hooks chứa toàn bộ logic
   const {
@@ -43,7 +45,7 @@ export default function CharacterWheel() {
       {/* Background */}
       <div
         className="absolute inset-0 bg-cover bg-center"
-        style={{ backgroundImage: "url('./assets/Backgrounds/wheel-bg.png')" }}
+        style={{ backgroundImage: `url(${wheelBg})` }}
       />
       <div className="absolute inset-0 bg-black/70" />
 
