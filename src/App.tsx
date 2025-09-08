37cc640c32faa796757de861ab6acf2e09afff6e
Nguyễn Quang Dương <furygod7@gmail.com>
2025-09-08 17:43:55 +0700

    nothing herre

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+import Menu from "./pages/Menu";
+import CharacterWheel from "./pages/CharacterWheel";
+
 import { ZackieProvider } from "./components/setResult.tsx";
-import Wheel from "./pages/Wheel.tsx";
-import Menu from "./pages/Menu.tsx";
-import CharacterWheel from "./pages/CharacterWheel.tsx";
 
 export default function App() {
   return (
-    <Router>
     <ZackieProvider>
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/wheel" element={<CharacterWheel />} />
       </Routes>
     </ZackieProvider>
-    </Router>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
-import App from "./App.tsx.tsx";
+import App from "./App.tsx";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
