2efce3fda3f7721284af90d7f5d1e1c20a441ee5
Nguyễn Quang Dương <furygod7@gmail.com>
2025-10-06 10:06:32 +0700

    database_version_one

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,32 +1,5 @@
 import { Sequelize,Op,DataTypes,QueryTypes } from "sequelize";
 import path from "path";
-import { PlayerTables } from "./tables/player";
-import { RaceTables } from "./tables/races";
-import { GearTables } from "./tables/gears";
-import { WeaponTables } from "./tables/weapons";
-import { PowerTables } from "./tables/power";
-import { QuirkTables } from "./tables/quirks";
-import { SubRaceTables } from "./tables/sub_race";
-import { legacyGearWheel } from "@/Common/Config/GearConfig";
-import { HouseTables } from "./tables/houses";
-import { ArchetypeTables } from "./tables/archetypes";
-import { PlayerArchetypeTables } from "./tables/player_archetypes";
-import { PlayerQuirkTables } from "./tables/player_quirks";
-import { PlayerGearTables } from "./tables/player_gears";
-import { PlayerWeaponTables } from "./tables/player_weapons";
-import { CharDevTables } from "./tables/char_dev";
-import { EnchantTables } from "./tables/enchants";
-import { WeaponEnchantTables } from "./tables/weapon_enchants";
-import { PlayerPowerTables } from "./tables/player_power";
-import { PveEnemiesTables } from "./tables/pve_enemies";
-import { TournamentPhaseTables } from "./tables/tournament_phase";
-import { EventEmitterAsyncResource } from "stream";
-import { EventTables } from "./tables/event";
-import { RewardTables } from "./tables/rewards";
-import { MatchTables } from "./tables/matches";
-import { MatchEventsTables } from "./tables/match_events";
-import { MatchRewardTables } from "./tables/match_reward";
-import { PlayerCharDevTables } from "./tables/player_char_dev";
 
 interface DbConnection {
   sequelize: Sequelize;
